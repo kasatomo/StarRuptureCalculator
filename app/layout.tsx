c1de256d0fa33938cde92646d5cc9b943be1ecf0
kasatomo <kasatomo0721@gmail.com>
2026-01-11 20:48:20 +0900

    mui setuo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,17 @@
 import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import "./globals.css";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "@/theme";
+import { CssBaseline } from "@mui/material";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto",
+});
 
 export const metadata: Metadata = {
   title: "[非公式]StarRuptureファンサイト - 計算ツール",
@@ -12,8 +24,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
-      <body className="antialiased">{children}</body>
+    <html lang="ja" className={roboto.variable}>
+      <body>
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
